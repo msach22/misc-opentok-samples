8c3f1bc4585314a81bc78193b9d7bb8a20018ab4
Ankur Oberoi <aoberoi@gmail.com>
2012-09-24 01:32:58 -0700

    removing credentials

---
+++ config.sample.js
@@ -0,0 +1,11 @@
+var apiKey = ""; // Your API Key
+var configSessions = {
+	"1" : {
+		"id" : "", // A generated session ID
+		"token" : "" // A generated token for this session ID
+		},
+	"2" : {
+		"id" : "", // A generated session ID
+		"token" : "" // A generated token for this session ID
+		}
+};
