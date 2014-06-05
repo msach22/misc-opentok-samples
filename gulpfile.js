db2fcfe467258891f9704dde191447c2d6727996
Ankur Oberoi <aoberoi@gmail.com>
2014-06-04 23:51:53 -0400

    uses latest opentok-layout-js

--- gulpfile.js
+++ gulpfile.js
@@ -22,17 +22,14 @@ require('pho-devstack')(gulp, {
     livereload: function() {
       return "<script>document.write('<script src=\"http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1\"></' + 'script>')</script>";
     }
-  }),
+  })
 
-  copy: [
-    'images/sprites/**/*',
-    'humans.txt',
-    'bower_components/angular/**/*.{js,map}',
-    'bower_components/jquery/**/*.{js,map}',
-    'bower_components/opentok-angular/opentok-angular.min.js',
-    'bower_components/opentok-layout-js/opentok-layout.min.js',
-    'opentok/**/*'
-  ]
 });
 
-// If needed, redefine tasks here
+var miscAssets = [
+  'src/bower_components/angular/**/*.{js,map}',
+  'src/bower_components/jquery/**/*.{js,map}',
+  'src/bower_components/opentok-angular/opentok-angular.min.js',
+  'src/bower_components/opentok-layout-js/opentok-layout.min.js',
+  'src/opentok/**/*'
+];
