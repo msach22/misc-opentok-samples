17d807784868cb719c702b47021ce3d57cb88da0
Song Zheng <song15@Songs-MacBook-Pro.local>
2014-09-24 16:56:09 -0700

    front end js styling

--- assets/js/customer.js
+++ assets/js/customer.js
@@ -1,36 +1,37 @@
-var dayDiff = 0;
-var DAYSTRING = "dddMMMDDYYYY";
+var dayDiff = 0,
+    DAYSTRING = 'dddMMMDDYYYY';
 
-function setDayAndAvail(){
+function setDayAndAvail() {
   var datestr = moment().add(dayDiff, 'days').format('ddd MMM DD, YYYY');
   var daystring = moment().add(dayDiff, 'days').format(DAYSTRING);
-  $("#dateHeader").text(datestr);
-  appHelper.getAvailability(daystring, function(bookings){
-    $(".time").addClass('selectableTime');
-    for(var i=0;i<bookings.length;i++){
-      var hour = moment(parseInt(bookings[i])).format('H');
-      $(".selectableTime.time[data-hour="+hour+"]").removeClass('selectableTime');
+  $('#dateHeader').text(datestr);
+  appHelper.getAvailability(daystring, function(bookings) {
+    $('.time').addClass('selectableTime');
+    for(var i=0;i<bookings.length;i++) {
+      var hour = moment(Number(bookings[i])).format('H');
+      $('.selectableTime.time[data-hour='+hour+']').removeClass('selectableTime');
     }
   });
 }
-$(".dateNavigate").click(function(){
-  var currentUTC = parseInt(moment().hour(0).minute(0).second(0).format('X'));
-  dayDiff = $(this).data('dir') === "left" ? dayDiff - 1 : dayDiff + 1;
+$('.dateNavigate').click(function() {
+  var currentUTC = Number(moment().hour(0).minute(0).second(0).format('X'));
+  dayDiff = $(this).data('dir') === 'left' ? dayDiff - 1 : dayDiff + 1;
   if(appHelper.getUTCAppointment(dayDiff, 0) < currentUTC) dayDiff = dayDiff + 1;
   setDayAndAvail();
 });
 
-$(".time").click(function(){
-  if(!$(this).hasClass("selectableTime")) return;
+$('.time').click(function() {
+  if(!$(this).hasClass('selectableTime')) return;
   var utc = appHelper.getUTCAppointment(dayDiff, $(this).data('hour'))*1000;
-  var timestring = appHelper.getUTCString(utc, "ddd MMM DD, YYYY") + " at " + appHelper.getUTCString(utc, "hA");
-  $("#appointmentTime").text(timestring);
+  var timestring = appHelper.getUTCString(utc, 'ddd MMM DD, YYYY') + ' at ' + appHelper.getUTCString(utc, 'hA');
+  $('#appointmentTime').text(timestring);
 
   // set input fields hidden
-  $("input[name=timestamp]").val(utc);
-  $("input[name=daystring]").val(appHelper.getUTCString(utc, DAYSTRING));
-  $("input[name=timestring]").val(timestring);
+  $('input[name=timestamp]').val(utc);
+  $('input[name=daystring]').val(appHelper.getUTCString(utc, DAYSTRING));
+  $('input[name=timestring]').val(timestring);
   $('#myModal').modal('show');
 });
 
 setDayAndAvail();
+
--- assets/js/rep.js
+++ assets/js/rep.js
@@ -3,49 +3,50 @@ var dayDiff = 0,
     ONE_HOUR = 3600000;
 
 // Templates
-var user_template = Handlebars.compile($("#user_value_template").html());
-var no_appointment_template = Handlebars.compile($("#no_appointment_template").html());
-var chatting_template = Handlebars.compile($("#chatting_template").html());
+var user_template = Handlebars.compile($("#user_value_template").html()),
+    no_appointment_template = Handlebars.compile($("#no_appointment_template").html()),
+    chatting_template = Handlebars.compile($("#chatting_template").html());
 
-function setDayAndAvail(){
+function setDayAndAvail() {
   var datestr = moment().add(dayDiff, 'days').format('ddd MMM DD, YYYY');
   var daystring = moment().add(dayDiff, 'days').format(DAYSTRING);
   $("#dateHeader").text(datestr);
-  appHelper.getAvailability(daystring, function(bookings){
+  appHelper.getAvailability(daystring, function(bookings) {
     $(".time").removeClass('bookedTime');
-    for(var i=0;i<bookings.length;i++){
-      var hour = moment(parseInt(bookings[i])).format('H');
+    for(var i=0;i<bookings.length;i++) {
+      var hour = moment(Number(bookings[i])).format('H');
       $(".selectableTime.time[data-hour="+hour+"]").addClass('bookedTime');
     }
   });
 }
 
-$(".dateNavigate").click(function(){
+$(".dateNavigate").click(function() {
   dayDiff = $(this).data('dir') === "left" ? dayDiff - 1 : dayDiff + 1;
   setDayAndAvail();
 });
 
-$(".time").click(function(){
+$(".time").click(function() {
   if(!$(this).hasClass("selectableTime")) return;
   var utc = appHelper.getUTCAppointment(dayDiff, $(this).data('hour'))*1000;
   var timestring = appHelper.getUTCString(utc, "ddd MMM DD, YYYY") + " at " + appHelper.getUTCString(utc, "hA");
 
-  $.get("/index.php/getinfo/"+utc, function(res){
+  $.get("/index.php/getinfo/"+utc, function(res) {
     var info = JSON.parse(res);
-    if(info){
+    if(info) {
       $("#rightContainer").html(user_template(info));
 
-      if(Math.abs(parseInt(info.Timestamp) - Date.now()) >= ONE_HOUR){
+      if(Math.abs(Number(info.Timestamp) - Date.now()) >= ONE_HOUR) {
+
         // appointment is too far away or has passed
         $("#rightContainer #startButton").hide();
       }
 
-      $("#startButton").click(function(){
+      $("#startButton").click(function() {
         startAppointment(info);
       });
-      $("#cancelButton").click(function(){
-        if(window.confirm("are you sure you want to cancel this appointment?")){
-          $.get("/index.php/cancel/"+utc, function(res){
+      $("#cancelButton").click(function() {
+        if(window.confirm("are you sure you want to cancel this appointment?")) {
+          $.get("/index.php/cancel/"+utc, function(res) {
             location.reload();
           });
         }
@@ -56,14 +57,15 @@ $(".time").click(function(){
   });
 });
 
-function startAppointment(info){
+function startAppointment(info) {
   info.url = location.protocol + "//" + location.host + "/index.php/chat/" + info.Sessionid;
   $("#rightContainer").html(chatting_template(info));
-  $("#stopAppointment").click(function(){
-    if(window.confirm("stop chatting?")){
+  $("#stopAppointment").click(function() {
+    if(window.confirm("stop chatting?")) {
       location.reload();
     }
   });
 }
 
 setDayAndAvail();
+
