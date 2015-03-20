afbf33f0bc241d4d50a22fb86b23b53370e97915
Roberto Pérez <rpc@tokbox.com>
2015-03-20 15:27:05 +0100

    Adding Xamarin Forms example

---
+++ Xamarin/Xamarin.Forms/OpenTokForms/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Configuration
+{
+	public static class Config
+	{
+		public const string API_KEY = "";
+		public const string SESSION_ID = "";
+		public const string TOKEN = "";
+	}
+}
+
